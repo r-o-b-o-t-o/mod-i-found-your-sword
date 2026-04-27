c4c1def6f4ac316471ff42f90dfefa5f372fe488
Axel Cocat <ax.cocat@gmail.com>
2026-04-28 01:11:03 +0200

    feat: remove dependencies

--- mod-i-found-your-sword.cmake
+++ mod-i-found-your-sword.cmake
@@ -1,7 +1,1 @@
-if (MSVC)
-    target_compile_options(modules PRIVATE /bigobj)
-endif ()
-
 target_include_directories(modules PUBLIC "mod-i-found-your-sword/libs")
-target_include_directories(modules PUBLIC "mod-i-found-your-sword/libs/wswrap")
-target_include_directories(modules PUBLIC "mod-i-found-your-sword/libs/asio")
